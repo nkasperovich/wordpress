f50c9595c9cd6cb711ccc99e73c9773059c5e23d
nkasperovich <57672537+nkasperovich@users.noreply.github.com>
2020-03-19 11:14:01 +0300

    Update save_feedback.php

--- simple-feedback/save_feedback.php
+++ simple-feedback/save_feedback.php
@@ -16,22 +16,25 @@ global $wpdb;
 
      $sql = "SELECT subject, email FROM wp_subjects WHERE id = {$subject_id}";
      $results = $wpdb->get_results($sql);
-     $subject = $results[0]->subject;
-     $subject_email = $results[0]->email;
-     $headers = array('Content-Type: text/html; charset=UTF-8');
+     
+     if (count($results)) {
+        $subject = $results[0]->subject;
+        $subject_email = $results[0]->email;
+        $headers = array('Content-Type: text/html; charset=UTF-8');
 
-     wp_mail( $subject_email, $subject, $message, $headers );
-
-     try {
-         $table_name = $wpdb->prefix . 'feedbacks';
-         $wpdb->insert($table_name, array(
-             'name' => $name,
-             'email' => $email,
-             'subject' => $subject,
-             'text' => $message
-         ));
-         echo 'Thank you for your feedback!';
-     } catch (Exception $e){
-         echo $e;
+        wp_mail( $subject_email, $subject, $message, $headers );
+      
+        try {
+           $table_name = $wpdb->prefix . 'feedbacks';
+           $wpdb->insert($table_name, array(
+               'name' => $name,
+               'email' => $email,
+               'subject' => $subject,
+               'text' => $message
+           ));
+           echo 'Thank you for your feedback!';
+        } catch (Exception $e){
+           echo $e;
+        }
      }
  }
